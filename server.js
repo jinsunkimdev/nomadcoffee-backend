6c12e13a61be83409cb0e2fbdcf88dc4aadb1c45
jinsunkim <jinsunkim94@gmail.com>
2022-03-30 03:13:48 +0900

    Set up project

---
+++ server.js
@@ -0,0 +1,17 @@
+import "dotenv/config";
+import { ApolloServer } from "apollo-server";
+import schema from "./schema";
+
+const server = new ApolloServer({
+  schema,
+});
+
+const PORT = process.env.PORT;
+
+server
+  .listen(PORT)
+  .then(() =>
+    console.log(
+      `👍 Server is running apollo-server graphql http://localhost:${PORT}/`
+    )
+  );
